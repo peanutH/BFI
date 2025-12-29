351770cf9f7e027f6ac058b883162536a34630c4
peanutH <40477854+peanutH@users.noreply.github.com>
2025-12-29 14:21:12 +0900

    update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://peanutH.github.io',
+    base: '/BFI',
+});
